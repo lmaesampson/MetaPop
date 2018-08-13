c6d2ee69a9b575e8ee1d653e9faa83723d3de319
lmaesampson <lmaesampson@gmail.com>
2018-08-13 11:07:51 -0400

    fixed gridsearch, removed old code

--- Code/gridsearch.py
+++ Code/gridsearch.py
@@ -7,8 +7,8 @@ import os
 import zipfile
 
 # Must be a more pythonic way:
-def callmp(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27):
-    call([a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27])
+def callmp(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25):
+    call([a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25])
 
 
 def run_process_limited_time(max_seconds,argdict):
